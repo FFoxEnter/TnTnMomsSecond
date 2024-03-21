9a69129d8c7b8397eb481de107b27f818846b924
GEUN SU <jgs@ffoxenter.com>
2024-03-21 17:18:12 +0900

    - 드래그 진행 중간 저장

--- Assets/02.Script/UI/Belly/UIDraggable.cs
+++ Assets/02.Script/UI/Belly/UIDraggable.cs
@@ -1,35 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class UIDraggable : MonoBehaviour
+public class Draggable : MonoBehaviour
 {
     public Camera targetCamera;
-    private bool isDragging;
-    private Vector3 mousePosition;
+    RaycastHit hit, hitFloor;
+    GameObject dragAnchor;
+    Vector3 mainCamPosition;
 
-    private void Update()
+    Vector3 ThisPosition;
+
+    void Start()
+    {
+        ThisPosition = transform.position;
+        mainCamPosition = targetCamera.transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        transform.SetParent(null);
+        Destroy(dragAnchor);
+        transform.position = ThisPosition;
+
+    }
+
+    private void OnMouseDown()
     {
         Ray ray = targetCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        mousePosition = Input.mousePosition;
-
-        if (Physics.Raycast(ray, out hit) && hit.transform == transform)
+        if (Physics.Raycast(ray, out hit))
         {
-            if (Input.GetMouseButtonDown(0))
+            dragAnchor = new GameObject("DragAnchor");
+            dragAnchor.transform.position = hit.point;
+            transform.SetParent(dragAnchor.transform);
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        Ray ray = targetCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hitFloor, Mathf.Infinity))
+        {
+            float h = dragAnchor.transform.position.y;
+
+            Vector3 camToFloor = hitFloor.point - targetCamera.transform.position;
+            Vector3 nextPosition = Vector3.zero;
+
+            float lo = 0.0f, hi = 1.0f; // ratio
+            for (int i = 0; i < 38; i++)
             {
-                isDragging = true;
-                Debug.Log("Raycast");
+                float diff = hi - lo;
+                float p1 = lo + diff / 3;
+                float p2 = hi - diff / 3;
+
+                var v1 = mainCamPosition + camToFloor * p1;
+                var v2 = mainCamPosition + camToFloor * p2;
+                if (Mathf.Abs(v1.y - h) > Mathf.Abs(v2.y - h))
+                {
+                    nextPosition = v2;
+                    lo = p1;
+                }
+                else
+                {
+                    nextPosition = v1;
+                    hi = p2;
+                }
             }
-        }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            isDragging = false;
-        }
-
-        if (isDragging)
-        {
-            Vector3 newPosition = targetCamera.ScreenToWorldPoint(Input.mousePosition);
-                Debug.Log("isDragging");
+            dragAnchor.transform.position = nextPosition;
         }
     }
 }
