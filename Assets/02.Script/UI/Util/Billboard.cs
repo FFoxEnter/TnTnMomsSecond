f886655e9fcab98068f8945cc2604faf227eb347
GEUN SU <jgs@ffoxenter.com>
2024-03-28 13:24:06 +0900

    - targetCamera를 Camera.main으로 변경

--- Assets/02.Script/UI/Util/Billboard.cs
+++ Assets/02.Script/UI/Util/Billboard.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    public Camera targetCamera;
+    public Camera MainCamera;
+
+    private void Awake()
+    {
+        SetMainCamera();
+    }
 
     void Update()
     {
-        transform.LookAt(transform.position + targetCamera.transform.rotation * Vector3.forward,
-            targetCamera.transform.rotation * Vector3.up);
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+            Camera.main.transform.rotation * Vector3.up);
+    }
+
+    void SetMainCamera()
+    {
+        MainCamera = Camera.main;
     }
 }
